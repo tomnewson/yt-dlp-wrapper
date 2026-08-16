bedc8eb897dba72794b37ba521d358949b20ea3d
Tom Newson <newsontom@outlook.com>
2026-08-16 21:18:40 +0100

    split files

--- build.rs
+++ build.rs
@@ -1,6 +1,6 @@
 fn main() {
     let config = slint_build::CompilerConfiguration::new().with_style("fluent-dark".into());
-    slint_build::compile_with_config("src/app.slint", config)
+    slint_build::compile_with_config("src/slint/app.slint", config)
         .expect("failed to compile the Slint UI");
 
     if std::env::var("CARGO_CFG_TARGET_OS").as_deref() == Ok("windows") {
